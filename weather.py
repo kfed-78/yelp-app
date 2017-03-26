a67b6de9e38bec5356716cab7ec1ea8daa29ef5c
kfed-78 <kdfox1@gmail.com>
2017-03-26 17:36:00 -0500

    make it better

--- weather.py
+++ weather.py
@@ -5,7 +5,6 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-
 def get_weather(address):
     api_key = os.environ['FORECASTIO_API_KEY']
     geolocator = Nominatim()
--- web.py
+++ web.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
+import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 import weather
-import os
+
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ def index():
 @app.route('/about')
 def about():
 	return render_template('about.html')
-	
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
